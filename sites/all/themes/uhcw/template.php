fc4ec47f6d613e33e4812c2b7d7ea3ecdf239353
benmango <ben@benmango.co.uk>
2018-05-21 16:35:42 +0200

    Updated functionality for different methodologies

--- sites/all/themes/uhcw/template.php
+++ sites/all/themes/uhcw/template.php
@@ -4,3 +4,24 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+function uhcw_preprocess_page(&$variables) {
+
+  $types = drupal_get_query_parameters();
+  $type = 'none';
+
+  if(drupal_is_front_page()) {
+    if(!isset($types['survey'])) {
+      drupal_set_message(t('There has been an error. Please check you have the correct link for the page.'), 'error');
+    }
+    else {
+      $survey = array('web', 'kiosk', 'mobile', 'paper');
+      if (!in_array($types['survey'], $survey)) {
+        drupal_set_message(t('There has been an error. Please check you have the correct link for the page.'),'error');
+      }
+    }
+  }
+
+
+  $variables['code'] = drupal_get_form('uhcw_form_code');
+}
